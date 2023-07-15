8b2ca7f32e9a80dd9648d6b97ba9d40d18fa80ac
Informatik User 3 <info-git3-tk@treptowkolleg.de>
2023-07-15 17:09:33 +0200

    update files

--- src/main/java/org/gamereact/controller/AppController.java
+++ src/main/java/org/gamereact/controller/AppController.java
@@ -10,6 +10,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import org.engine.AppTimer;
 import org.engine.ButtonConfig;
@@ -40,7 +41,12 @@ public class AppController extends AppTimer implements Initializable, TuioListen
     private final FadeTransition ft = Transitions.createFadeTransition(2000,middleCircle,0,.5);
     private final TuioClient client = new TuioClient();
 
+    private final Group layoutGroup = new Group();
+    private final Group objectGroup = new Group();
+    private final Group cursorGroup = new Group();
+
     private final HashMap<TuioCursor, Circle> cursorList = new HashMap<>();
+    private final HashMap<TuioObject, Group> objectList = new HashMap<>();
 
     /**
      * Setup des Controllers, sobald die Stage vollständig verarbeitet wurde.
@@ -58,16 +64,21 @@ public class AppController extends AppTimer implements Initializable, TuioListen
         client.addTuioListener(this);
         client.connect();
 
+
+
         this.transitionCircle.setFill(Color.TRANSPARENT);
         this.transitionCircle.setStroke(new Color(1,1,1,.4));
         this.transitionCircle.setStrokeWidth(.1);
 
         this.infoText.setTranslateX(10);
-        this.root.getChildren().add(this.infoText);
 
+        this.layoutGroup.getChildren().add(this.infoText);
+        this.layoutGroup.getChildren().add(this.transitionCircle);
+        this.layoutGroup.getChildren().add(this.middleCircle);
 
-        this.root.getChildren().add(this.transitionCircle);
-        this.root.getChildren().add(this.middleCircle);
+        this.root.getChildren().add(layoutGroup);
+        this.root.getChildren().add(objectGroup);
+        this.root.getChildren().add(cursorGroup);
 
 
         root.setStyle("-fx-background-color: #005EAA");
@@ -125,18 +136,28 @@ public class AppController extends AppTimer implements Initializable, TuioListen
     }
 
     private void getTangibleInput() {
+
+        for ( Map.Entry<TuioObject,Group> object:
+                this.objectList.entrySet()) {
+            if(!this.objectGroup.getChildren().contains(object.getValue())) this.objectGroup.getChildren().add(object.getValue());
+            int cx = object.getKey().getScreenX((int) this.root.getWidth());
+            int cy = object.getKey().getScreenY((int) this.root.getHeight());
+            object.getValue().setTranslateX(cx);
+            object.getValue().setTranslateY(cy);
+            object.getValue().getChildren().get(0).setRotate(object.getKey().getAngleDegrees());
+        }
+        this.objectGroup.getChildren().retainAll(this.objectList.values());
+
         for ( Map.Entry<TuioCursor,Circle> cursor:
                 this.cursorList.entrySet()) {
+            if(!this.cursorGroup.getChildren().contains(cursor.getValue())) this.cursorGroup.getChildren().add(cursor.getValue());
             int cx = cursor.getKey().getScreenX((int) this.root.getWidth());
             int cy = cursor.getKey().getScreenY((int) this.root.getHeight());
-            if(!this.root.getChildren().contains(cursor.getValue())) this.root.getChildren().add(cursor.getValue());
-
             cursor.getValue().setTranslateX(cx);
             cursor.getValue().setTranslateY(cy);
         }
+        this.cursorGroup.getChildren().retainAll(this.cursorList.values());
 
-        this.root.getChildren().retainAll(this.cursorList.values());
-        this.root.getChildren().addAll(this.transitionCircle,this.middleCircle);
     }
 
     /*
@@ -167,6 +188,28 @@ public class AppController extends AppTimer implements Initializable, TuioListen
     @Override
     public void addTuioObject(TuioObject tobj) {
         System.out.println("Object added!");
+        Group group = new Group();
+        Rectangle rectangle = new Rectangle(80,80,new Color(0.4,0.6,0.8,1));
+        Circle circle = new Circle(20,new Color(0.3,0.8,0.9,1));
+        Text idText = new Text(String.valueOf(tobj.getSymbolID()));
+        idText.setTranslateX(70);
+        rectangle.setArcWidth(20);
+        rectangle.setArcHeight(20);
+        rectangle.setTranslateX(-40);
+        rectangle.setTranslateY(-40);
+        group.getChildren().add(rectangle);
+        group.getChildren().add(circle);
+        group.getChildren().add(idText);
+
+
+        ScaleTransition cst = Transitions.createScaleTransition(50,group,.5,1);
+        ScaleTransition cst2 = Transitions.createScaleTransition(100,circle,0,3);
+        FadeTransition cft2 = Transitions.createFadeTransition(200,circle,1,0,1);
+        this.objectList.put(tobj,group);
+        cst.play();
+        cst2.play();
+        cft2.play();
+        cft2.setOnFinished(e -> group.getChildren().remove(circle));
     }
 
     @Override
@@ -177,6 +220,7 @@ public class AppController extends AppTimer implements Initializable, TuioListen
     @Override
     public void removeTuioObject(TuioObject tobj) {
         System.out.println("Object removed!");
+        this.objectList.remove(tobj);
     }
 
     @Override
@@ -216,7 +260,6 @@ public class AppController extends AppTimer implements Initializable, TuioListen
 
     @Override
     public void refresh(TuioTime ftime) {
-
     }
 
     /*
