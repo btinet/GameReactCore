e6943922391a75170d06d8d8af5b02b50363721c
Benjamin Wagner <51082373+btinet@users.noreply.github.com>
2023-07-29 10:42:07 +0200

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'GameReact Core'
+copyright = '2022-2023, Benjamin Wagner'
+author = 'Benjamin Wagner'
 
 release = '0.1'
 version = '0.1.0'
